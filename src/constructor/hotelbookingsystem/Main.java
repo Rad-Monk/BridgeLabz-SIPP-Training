26eac1684f86442ebabfbd4da9a28862fb1625ba
Rad_Monk <radmonk8979@gmail.com>
2025-06-28 09:19:53 +0530

    [Aryans added main for hotel booking system in constructor

---
+++ src/constructor/hotelbookingsystem/Main.java
@@ -0,0 +1,26 @@
+package constructor.hotelbookingsystem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Using default constructor
+		HotelBooking defaultBooking = new HotelBooking();
+		System.out.println("Default Booking:");
+		defaultBooking.displayBooking();
+
+		System.out.println();
+
+		// Using parameterized constructor
+		HotelBooking customBooking = new HotelBooking("Ayush", "Deluxe", 2);
+		System.out.println("Custom Booking:");
+		customBooking.displayBooking();
+
+		System.out.println();
+
+		// Using copy constructor
+		HotelBooking copiedBooking = new HotelBooking(customBooking);
+		System.out.println("Copied Booking:");
+		copiedBooking.displayBooking();
+	}
+
+}
