67800047cdaf72981cdc560deeafa4ed67f6d6a2
Rad_Monk <radmonk8979@gmail.com>
2025-06-28 09:26:06 +0530

    [Aryan] added post graduate student in constructor

---
+++ src/constructor/universitymanagementsystem/PostgraduateStudent.java
@@ -0,0 +1,22 @@
+package constructor.universitymanagementsystem;
+
+class PostgraduateStudent extends Student {
+    private String specialization;
+
+    // Constructor for PostgraduateStudent
+    public PostgraduateStudent(int rollNumber, String name, double cgpa, String specialization) {
+        super(rollNumber, name, cgpa);
+        this.specialization = specialization;
+    }
+
+    // Demonstrating use of protected member "name"
+    public void welcomeMessage() {
+        System.out.println("Welcome postgraduate student, " + name + "!");
+    }
+
+    // Method to display postgraduate student details
+    public void showDetails() {
+        displayInfo();
+        System.out.println("Specialization: " + specialization);
+    }
+}
