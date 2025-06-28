fb28343cf9981276a2e39236d2caab80f8fa7994
Rad_Monk <radmonk8979@gmail.com>
2025-06-28 09:21:22 +0530

    [Aryan] added main for productive inventory in constructor

---
+++ src/constructor/productinventory/Main.java
@@ -0,0 +1,25 @@
+package constructor.productinventory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Create instances of Product
+		// Each instance will automatically increment the totalProducts count
+		Product p1 = new Product("Laptop", 999.99);
+		Product p2 = new Product("Smartphone", 499.49);
+		Product p3 = new Product("Headphones", 89.99);
+
+		// Display product details and total products created
+		System.out.println("Product Details:\n");
+		p1.displayProductDetails();
+		System.out.println();
+		p2.displayProductDetails();
+		System.out.println();
+		p3.displayProductDetails();
+
+		// Display total number of products created
+		System.out.println();
+		Product.displayTotalProducts();
+	}
+
+}
