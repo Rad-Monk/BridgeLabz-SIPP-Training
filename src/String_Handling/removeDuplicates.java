679191640d95cf9ad734f8741f56c24dd3861923
Rad_Monk <radmonk8979@gmail.com>
2025-06-28 09:47:35 +0530

    [Aryan] added program for remove duplicates

---
+++ src/String_Handling/removeDuplicates.java
@@ -0,0 +1,13 @@
+package String_Handling;
+
+public class removeDuplicates {
+    public static void main(String[] args) {
+        String str = "programming";
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (result.indexOf(ch) == -1) result += ch;
+        }
+        System.out.println("Modified String: " + result);
+    }
+}
