7dbef5a081696ea9da81d3133a9ed616b6b73c4b
Rad_Monk <radmonk8979@gmail.com>
2025-06-28 09:22:27 +0530

    [Aryan] added main for employee management system in keywords

---
+++ src/keywords/employeemanagementsystem/Main.java
@@ -0,0 +1,27 @@
+package keywords.employeemanagementsystem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Creating employee objects
+		Employee emp1 = new Employee("Alice", 101, "Software Engineer");
+		Employee emp2 = new Employee("Bob", 102, "Project Manager");
+
+		// Checking with instanceof and displaying details
+		if (emp1 instanceof Employee) {
+			emp1.displayEmployeeDetails();
+		}
+
+		System.out.println(); // spacing
+
+		if (emp2 instanceof Employee) {
+			emp2.displayEmployeeDetails();
+		}
+
+		System.out.println(); // spacing
+
+		// Display total number of employees
+		Employee.displayTotalEmployees();
+	}
+
+}
