0db54fb0152c6461f96695db0440ac5dbb21ad95
Rad_Monk <radmonk8979@gmail.com>
2025-07-11 11:18:45 +0530

    [Aryan] inheritance example for animal

---
+++ src/inheritance/introduction/animalhierarchy/Animal.java
@@ -0,0 +1,9 @@
+package com.inheritance.introduction.animalhierarchy;
+
+public class Animal {
+	protected String name;
+	protected int age;
+	protected void makeSound() {
+		System.out.println("Animal make sounds");
+	}
+}
