943eb1656018651aa62fd84eaaf349b2f861193c
Rad_Monk <radmonk8979@gmail.com>
2025-07-15 09:10:49 +0530

    [Aryan] principles example in banking system

---
+++ src/principles/bankingsystem/SavingsAccount.java
@@ -0,0 +1,24 @@
+package com.principles.bankingsystem;
+
+class SavingsAccount extends BankAccount implements Loanable {
+	private static final double INTEREST_RATE = 0.04; // 4% annual
+
+	public SavingsAccount(String accountNumber, String holderName, double balance) {
+		super(accountNumber, holderName, balance);
+	}
+
+	@Override
+	public double calculateInterest() {
+		return getBalance() * INTEREST_RATE;
+	}
+
+	@Override
+	public void applyForLoan(double amount) {
+		System.out.println("Loan of ₹" + amount + " applied successfully for Savings Account.");
+	}
+
+	@Override
+	public double calculateLoanEligibility() {
+		return getBalance() * 2; // Eligibility = 2 times the balance
+	}
+}
