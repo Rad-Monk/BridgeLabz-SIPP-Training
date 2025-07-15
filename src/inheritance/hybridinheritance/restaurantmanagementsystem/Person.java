256703aaaab52aec49d3c53d5dff8dd1e5dbcc9b
Rad_Monk <radmonk8979@gmail.com>
2025-07-15 09:01:35 +0530

    [Aryan] inheritance example in restaurant management system

---
+++ src/inheritance/hybridinheritance/restaurantmanagementsystem/Person.java
@@ -0,0 +1,15 @@
+package com.inheritance.hybridinheritance.restaurantmanagementsystem;
+
+class Person {
+   private String name;
+   private int id;
+
+   public Person(String name, int id) {
+      this.name = name;
+      this.id = id;
+   }
+
+   public void displayInfo() {
+      System.out.println("Name: " + name + ", ID: " + id);
+   }
+}
