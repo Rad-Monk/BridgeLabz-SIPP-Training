5c82279ee7fa4e966c750e5b91100de61f62705b
Rad_Monk <radmonk8979@gmail.com>
2025-07-15 08:59:25 +0530

    [Aryan] main in inheritance in library management system

---
+++ src/inheritance/singleinheritance/librarymanagement/Main.java
@@ -0,0 +1,8 @@
+package com.inheritance.singleinheritance.librarymanagement;
+
+public class Main {
+    public static void main(String[] args) {
+        Author a = new Author("Clean Code", 2008, "Robert C. Martin", "Uncle Bob is a software engineer and author.");
+        a.displayInfo();
+    }
+}
