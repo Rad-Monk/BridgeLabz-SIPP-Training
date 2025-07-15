79dd06c78d5ab890c7b463c724c8ffd6938aae3c
Rad_Monk <radmonk8979@gmail.com>
2025-07-15 09:05:34 +0530

    [Aryan] principles example in e employee management system

---
+++ src/principles/employeemanagementsystem/Employee.java
@@ -0,0 +1,46 @@
+package com.principles.employeemanagementsystem;
+
+public abstract class Employee {
+    private int employeeId;
+    private String name;
+    private double baseSalary;
+
+    // Constructor
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    // Getters and Setters (Encapsulation)
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary > 0) {
+            this.baseSalary = baseSalary;
+        } else {
+            System.out.println("Base salary must be positive.");
+        }
+    }
+
+    // Abstract method
+    public abstract double calculateSalary();
+
+    // Concrete method
+    public void displayDetails() {
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Name: " + name);
+        System.out.println("Base Salary: $" + baseSalary);
+        System.out.println("Total Salary: $" + calculateSalary());
+    }
+}
