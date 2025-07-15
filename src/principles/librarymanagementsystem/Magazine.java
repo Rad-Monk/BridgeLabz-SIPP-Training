63a3f2bdc70ad4a1dd96e35e5f1e6202e61b2de8
Rad_Monk <radmonk8979@gmail.com>
2025-07-15 09:08:07 +0530

    [Aryan] principles example in library management system

---
+++ src/principles/librarymanagementsystem/Magazine.java
@@ -0,0 +1,12 @@
+package com.principles.librarymanagementsystem;
+
+class Magazine extends LibraryItem {
+ public Magazine(String itemId, String title, String author) {
+     super(itemId, title, author);
+ }
+
+ @Override
+ public int getLoanDuration() {
+     return 7;  // 7 days loan period
+ }
+}
